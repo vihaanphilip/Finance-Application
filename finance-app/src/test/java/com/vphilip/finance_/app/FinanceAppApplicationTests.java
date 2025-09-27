6a6d8c05258901dde3bf47a36cf807def9996f3a
Vihaan Philip <vihaanphilip@gmail.com>
2025-09-27 15:05:17 +0800

    feat: spring boot project starter

---
+++ finance-app/src/test/java/com/vphilip/finance_/app/FinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vphilip.finance_.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
