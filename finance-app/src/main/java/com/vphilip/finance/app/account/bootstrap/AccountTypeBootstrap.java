66322629fd8610a4cadc88c25e36edc0f679eeb2
Vihaan Philip <vihaanphilip@gmail.com>
2025-10-04 00:39:53 +0800

    feat(property): Set a property to toggle botostrapping

--- finance-app/src/main/java/com/vphilip/finance/app/account/bootstrap/AccountTypeBootstrap.java
+++ finance-app/src/main/java/com/vphilip/finance/app/account/bootstrap/AccountTypeBootstrap.java
@@ -7,6 +7,7 @@ import com.vphilip.finance.app.account.model.AccountTypes;
 import com.vphilip.finance.app.account.repository.AccountTypeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,9 @@ import java.io.InputStream;
 
 @Component
 public class AccountTypeBootstrap implements CommandLineRunner {
+
+    @Value("${app.bootstrap-data:false}")
+    private boolean bootstrapEnabled;
 
     private static final Logger log = LoggerFactory.getLogger(AccountTypeBootstrap.class);
     private final ObjectMapper objectMapper;
@@ -27,6 +31,11 @@ public class AccountTypeBootstrap implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        if (!bootstrapEnabled) {
+            log.info("AccountTypeBootstrap is disabled. Skipping data loading.");
+            return;
+        }
+        log.info("AccountTypeBootstrap is enabled. Loading data...");
         try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/account_types.json")) {
             AccountTypes allAccountTypes = objectMapper.readValue(inputStream, AccountTypes.class);
             log.info("Reading {} runs from JSON data and saving to in-memory collection.", allAccountTypes.accountTypes().size());
